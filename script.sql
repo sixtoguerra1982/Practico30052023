011ce124bcdd625e1303c0a1bc59e8df18449857
Sandra González <s-agonzalez@hotmail.com>
2023-05-31 23:24:59 -0400

    ultima version

--- script.sql
+++ script.sql
@@ -1,11 +1,11 @@
 -- 0
 DROP DATABASE blog;
---Creamos la base de datos
+-- 1.Creamos la base de datos
 CREATE DATABASE blog;
---Nos conectamos a la base de datos
+-- 1.1 Nos conectamos a la base de datos
 \c blog
 --estamos dentro de la base de datos prompt
---creamos la tabla
+-- 2.creamos la tabla
 --Primero deben ir las entidades fuertes: las que no requieren de otro para poder existir
 --Luego deben ir las entidades secundarias: las que son dependientes/hijas de las primeras entidades
 ---Creamos la estrucutura
@@ -38,12 +38,12 @@ CREATE TABLE comentario(
 
 --3. Insertar los siguientes registros.  
 
-\copy usuario FROM '/home/sixtolio/Escritorio/Proyectos_JS/Modulo5/Grupal/usuario.csv' csv HEADER;
+\copy usuario FROM 'C:\Users\56954\Desktop\trabajo\Practico30052023\usuario.csv' csv HEADER;
 
 
-\copy post FROM '/home/sixtolio/Escritorio/Proyectos_JS/Modulo5/Grupal/post.csv' csv HEADER;
+\copy post FROM 'C:\Users\56954\Desktop\trabajo\Practico30052023\post.csv' csv HEADER;
 
-\copy comentario FROM '/home/sixtolio/Escritorio/Proyectos_JS/Modulo5/Grupal/comentario.csv' csv HEADER;
+\copy comentario FROM 'C:\Users\56954\Desktop\trabajo\Practico30052023\comentario.csv' csv HEADER;
 
 
 --4 Seleccionar el correo, id y título de todos los post publicados por el usuario 5.
@@ -54,7 +54,7 @@ SELECT * FROM post WHERE usuario_id = 5;
 --que no hayan sido realizados por el usuario con email ​usuario06@hotmail.com​
 
 SELECT u.id as user_id, u.email, c.id, c.texto FROM comentario AS c INNER JOIN usuario AS u ON c.usuario_id = u.id 
-WHERE u.id <> 6;
+WHERE u.email <> 'usuario06@hotmail.com';
 
 
 
@@ -64,6 +64,10 @@ WHERE u.id <> 6;
 
 SELECT * FROM usuario FULL JOIN post ON usuario.id = post.usuario_id WHERE post.usuario_id IS NULL;
 
+-- 7. Mostrar todos los post con sus comentarios, incluyendo aquellos que no poseen comentarios
+SELECT * FROM post p FULL OUTER JOIN comentario c ON p.id = c.post_id ORDER BY p.id;
 
+-- 8. Listar todos los usuarios que hayan publicado un post en junio
+SELECT * FROM usuario INNER JOIN post ON post.usuario_id = usuario.id WHERE fecha BETWEEN '2020-06-01' AND '2020-06-30';
 
 
